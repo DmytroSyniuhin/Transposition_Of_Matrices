2c271ebb9cd20e0c1fa9ce36c29d05986e80aa3c
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2023-10-22 13:45:35 +0300

    Homework

--- src/TranspositionOfMatrices.java
+++ src/TranspositionOfMatrices.java
@@ -22,21 +22,27 @@ public class TranspositionOfMatrices {
         }
         System.out.println("Матриця розміром M×N:");
 
-        for (int i = 0; i < matrix1.length; i++) {
-            for (int j = 0; j < matrix1[i].length; j++) {
-                System.out.print(matrix1[i][j] + ", ");
+        {
+            int i = 0;
+            while (i < matrix1.length) {
+                for (int j = 0; j < matrix1[i].length; j++) {
+                    System.out.print(matrix1[i][j] + ", ");
+                }
+                System.out.println();
+                i++;
             }
-            System.out.println();
         }
 
         System.out.println();
 
         System.out.println("Матриця розміром N×M:");
-        for (int j = 0; j < matrix2.length; j++) {
+        int j = 0;
+        while (j < matrix2.length) {
             for (int i = 0; i < matrix2[j].length; i++) {
                 System.out.print(matrix2[j][i] + ", ");
             }
             System.out.println();
+            j++;
         }
     }
 }
